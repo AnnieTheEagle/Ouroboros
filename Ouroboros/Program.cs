ef2a01b4c7ed3f703cdadb5221c7775297e9b15c
Annie the Eagle <falconer2010@gmail.com>
2015-06-27 14:57:36 +0100

    * Restructured Code and removed CardCount.cs (deprecated). * Fixed App Version Variable.

--- Ouroboros/Program.cs
+++ Ouroboros/Program.cs
@@ -8,7 +8,7 @@ using System.Windows.Forms;
 
 namespace Ouroboros {
     public static class Program {
-        public static const string APP_VERSION = "1.10";
+        public const string APP_VERSION = "1.10";
 
         [STAThread]
         public static void Main() { // The main entry point for the application.
